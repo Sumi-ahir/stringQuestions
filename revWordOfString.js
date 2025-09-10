3bdfbf7610766fe90e62eb3d7fc58eb2d362b52b
Sumi'Stake <sumi.sparkal@gmail.com>
2025-09-10 18:49:35 +0530

    Add files via upload
    # Reverse String Word Wise
    
    ##  Problem Statement
    Given a sentence, reverse it **word-wise** (reverse the order of words, but keep the characters of each word as it is).

---
+++ revWordOfString.js
@@ -0,0 +1,10 @@
+function revStringWord(str){
+
+    let word=str.split(" ");
+    let reversed=word.reverse();
+    return reversed.join(" ");
+    
+}console.log(revStringWord("I Love Coding "));
+console.log(revStringWord("I am dreamer"));
+
+
