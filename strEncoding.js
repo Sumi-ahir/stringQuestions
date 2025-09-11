18771f8d8d02581d1255c714c7eacb5a0bba13b6
Sumi'Stake <sumi.sparkal@gmail.com>
2025-09-11 10:46:37 +0530

    Add files via upload
    Encoding the string

---
+++ strEncoding.js
@@ -0,0 +1,15 @@
+function strEncoding(str){
+    let result="";
+    let count=1;
+    for(let i=0;i<str.length;i++){
+        if(str[i]===str[i+1] ){
+            count++;
+        }else{
+            // result=str[i]+count;
+            result+=str[i]+count;
+            count=1;
+        }
+    }return result;
+}
+console.log(strEncoding("ccccc"));
+console.log(strEncoding("aaabbbcc"));
